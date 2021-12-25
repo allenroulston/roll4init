609fa04ce3aebba4bd86c8eeaad20db1adf53aa4
Allen Roulston <allenroulston@users.noreply.github.com>
2021-12-25 11:01:48 -0400

    revised the App Token info

---
+++ roll4init.rb
@@ -0,0 +1,24 @@
+#
+require 'discordrb';
+require 'yaml';
+require 'date';
+require 'securerandom';
+include Math;
+
+
+##########  Configuration  ########
+junk = YAML.load(File.read("roll4initData.yml"));
+token = junk[0]+junk[1]+junk[2];
+
+prefix = "!" # Your bot's prefix
+
+owner = 924129933586997308  # the Application ID
+
+puts;
+puts "          Roll 4 Init will be active soon"
+puts;
+
+initiativeBase = YAML.load(File.read("initiativeBase.yml"));
+
+
+bot.run
