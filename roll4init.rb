149d72e7c93e76d333d258079f4633700faafe3c
Allen Roulston <allenroulston@users.noreply.github.com>
2021-12-29 21:36:43 -0400

    added two methods
    !a & !d
    to add Alive and Dead functions

--- roll4init.rb
+++ roll4init.rb
@@ -153,5 +153,52 @@ bot.message(start_with: "!n") do |event|;
   File.open("activeInit.yml", 'w+') {|f| f.write(data) }; 
   event.respond say;
 end;
-
+##################################################################################################################
+##################################################################################################################
+bot.message(start_with: "!d") do |event|;
+   event.message.delete; 
+   activeInitiative = YAML.load(File.read("initiativeBase.yml"));
+   howMany = (activeInitiative.length)-1;
+   deaders = event.content.slice(2,99);
+   data = "---" + "\n";
+   (0..howMany).each do |x|;
+       theZero = activeInitiative[x][0].to_s;
+       one = activeInitiative[x][1].to_s;
+       two = activeInitiative[x][2].to_s;
+       three = activeInitiative[x][3].to_s
+       four = activeInitiative[x][4].to_s;
+       five = activeInitiative[x][5].to_s;
+       if deaders.index(two) != nil then;
+         three = "Dead";
+       end;
+       data = data + '- [' + theZero + ',' + one + ',"' + two + '","' + three + '",' + four + ',"' + five + '"]' +  "\n";
+    #   format of:     - ["Y", 0, "Z", "alive", 0, "mt"]
+   end;
+   File.open("initiativeBase.yml", 'w+') {|f| f.write(data) };
+   event.respond "Dead complete for " + deaders;
+end;
+##################################################################################################################
+##################################################################################################################
+bot.message(start_with: "!a") do |event|;
+   activeInitiative = YAML.load(File.read("initiativeBase.yml"));
+   howMany = (activeInitiative.length)-1;
+   livers = event.content.slice(2,99);
+   data = "---" + "\n";
+   (0..howMany).each do |x|;
+       theZero = activeInitiative[x][0].to_s;
+       one = activeInitiative[x][1].to_s;
+       two = activeInitiative[x][2].to_s;
+       three = activeInitiative[x][3].to_s
+       four = activeInitiative[x][4].to_s;
+       five = activeInitiative[x][5].to_s;
+       if livers.index(two) != nil then;
+         three = "Alive";
+       end;
+       data = data + '- [' + theZero + ',' + one + ',"' + two + '","' + three + '",' + four + ',"' + five + '"]' +  "\n";
+    #   format of:     - ["Y", 0, "Z", "alive", 0, "mt"]
+   end;
+   File.open("initiativeBase.yml", 'w+') {|f| f.write(data) };
+   event.respond "Live complete for " + livers;
+end;
+#####################################################################
 bot.run
