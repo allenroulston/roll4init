47d94efb98d293945817dda04b80e701dba05add
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-06 19:43:10 -0400

    new use of FILTER

--- roll4init.rb
+++ roll4init.rb
@@ -48,7 +48,7 @@ bot.message(start_with: "piss") do |event|;
             :user => ENV['RETHINKDB_USERNAME'] || 'admin',
             :password => ENV['RETHINKDB_PASSWORD'] || '',
             :db => ENV['RETHINKDB_NAME'] || 'test', ).repl
-  bob = r.table('hitPoints').get_all('name').run #(conn)
+  bob = r.table('hitPoints').filter({'name' => 'A'}).run #(conn)
   say = say + bob.inspect;
   event.respond say;  
 end;
