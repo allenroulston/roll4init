3317e28352ef8e7ba8384067352a1285e6862a89
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-06 18:56:32 -0400

    new load of database with dataBaseTest.rb

--- dataBaseTest.rb
+++ dataBaseTest.rb
@@ -6,7 +6,16 @@ r.connect(:host => ENV['RETHINKDB_HOST'] || 'localhost',
           :user => ENV['RETHINKDB_USERNAME'] || 'admin',
           :password => ENV['RETHINKDB_PASSWORD'] || '',
           :db => ENV['RETHINKDB_NAME'] || 'test', ).repl
-r.tableDrop('hitPoints').run
-#r.table('hitPoints').insert({ 'name'=>'A' }).run
-#r.table('hitPoints').insert({ 'name'=>'B' }).run
-#r.table('hitPoints').insert({ 'name'=>'C' }).run
+r.table_create('hitPoints').run
+r.table('hitPoints').insert({ 'name'=>'A', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'B', 'current'=>'95', 'max'=>'95' }).run
+r.table('hitPoints').insert({ 'name'=>'C', 'current'=>'90', 'max'=>'90' }).run
+r.table('hitPoints').insert({ 'name'=>'D', 'current'=>'85', 'max'=>'85' }).run
+r.table('hitPoints').insert({ 'name'=>'E', 'current'=>'80', 'max'=>'80' }).run
+r.table('hitPoints').insert({ 'name'=>'F', 'current'=>'75', 'max'=>'75' }).run
+r.table('hitPoints').insert({ 'name'=>'G', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'H', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'I', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'J', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'K', 'current'=>'100', 'max'=>'100' }).run
+r.table('hitPoints').insert({ 'name'=>'L', 'current'=>'100', 'max'=>'100' }).run
