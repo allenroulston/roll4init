c614a6df2f10dffe84e63c023826c284df593301
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-06 18:49:35 -0400

    new? file dataBaseTest.rb

--- dataBaseTest.rb
+++ dataBaseTest.rb
@@ -6,7 +6,7 @@ r.connect(:host => ENV['RETHINKDB_HOST'] || 'localhost',
           :user => ENV['RETHINKDB_USERNAME'] || 'admin',
           :password => ENV['RETHINKDB_PASSWORD'] || '',
           :db => ENV['RETHINKDB_NAME'] || 'test', ).repl
-r.table_create('hitPoints').run
-r.table('hitPoints').insert({ 'name'=>'A' }).run
-r.table('hitPoints').insert({ 'name'=>'B' }).run
-r.table('hitPoints').insert({ 'name'=>'C' }).run
+r.table_drop('hitPoints').run
+#r.table('hitPoints').insert({ 'name'=>'A' }).run
+#r.table('hitPoints').insert({ 'name'=>'B' }).run
+#r.table('hitPoints').insert({ 'name'=>'C' }).run
