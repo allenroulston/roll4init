c0aef2a39e585a6ad9289a70e7129a43b647bded
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-07 12:37:03 -0400

    update DMG method

--- roll4init.rb
+++ roll4init.rb
@@ -85,7 +85,7 @@ end;
 bot.message(start_with: "DMG") do |event|;
      say = "This is a DAMAGE result \n";
   letter = event.content.slice(3,1);
-  hurt = event.content.slice(5,9).to_i;
+  hurt = event.content.slice(4,9).to_i;
   say = say + letter + "   " + hurt.to_s + "\n";
   r.connect(:host => ENV['RETHINKDB_HOST'] || 'localhost',
             :port => ENV['RETHINKDB_PORT'] || 28015,
