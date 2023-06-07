f795843e198690fc640786c9be1a9b89d154a700
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-07 14:58:46 -0400

    mods to dork method

--- roll4init.rb
+++ roll4init.rb
@@ -111,7 +111,7 @@ bot.message(start_with: "dork") do |event|;
     currentHP = doc["val"][1].to_i;           
   end;
   # val 0 is maximum and val 1 is current hp
-  say = say + "DEDUCTING HP:" + hurt.to_s + "  from " +  current.to_s + " HP ";
+  say = say + "DEDUCTING HP:" + hurt.to_s + "  from " +  currentHP.to_s + " HP ";
   r.table('hitPoints').update({ :id => theID, 'val'=>[maxHP,currentHP] }).run
 end;  
 ##################################################################################################################
