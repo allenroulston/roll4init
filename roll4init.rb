186eb846f196bcdb0f7815c23d70044848d7dfbd
Allen Roulston <allenroulston@users.noreply.github.com>
2023-06-07 00:05:16 -0400

    added more :name letters
    A to Z

--- roll4init.rb
+++ roll4init.rb
@@ -50,8 +50,32 @@ bot.message(start_with: "SHIT") do |event|;
    r.table_drop('hitPoints').run
    r.table_create('hitPoints').run
    r.table('hitPoints').insert({ :name=>'A', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'B', 'val'=>[90,85] }).run
-   r.table('hitPoints').insert({ :name=>'C', 'val'=>[80,75] }).run
+   r.table('hitPoints').insert({ :name=>'B', 'val'=>[95,90] }).run
+   r.table('hitPoints').insert({ :name=>'C', 'val'=>[90,85] }).run
+   r.table('hitPoints').insert({ :name=>'D', 'val'=>[85,80] }).run
+   r.table('hitPoints').insert({ :name=>'E', 'val'=>[80,75] }).run
+   r.table('hitPoints').insert({ :name=>'F', 'val'=>[75,70] }).run
+   r.table('hitPoints').insert({ :name=>'G', 'val'=>[100,95] }).run
+   r.table('hitPoints').insert({ :name=>'H', 'val'=>[95,90] }).run
+   r.table('hitPoints').insert({ :name=>'I', 'val'=>[90,85] }).run
+   r.table('hitPoints').insert({ :name=>'J', 'val'=>[85,80] }).run
+   r.table('hitPoints').insert({ :name=>'K', 'val'=>[80,75] }).run
+   r.table('hitPoints').insert({ :name=>'L', 'val'=>[75,70] }).run
+   r.table('hitPoints').insert({ :name=>'M', 'val'=>[100,95] }).run
+   r.table('hitPoints').insert({ :name=>'N', 'val'=>[95,90] }).run
+   r.table('hitPoints').insert({ :name=>'O', 'val'=>[90,85] }).run
+   r.table('hitPoints').insert({ :name=>'P', 'val'=>[85,80] }).run
+   r.table('hitPoints').insert({ :name=>'Q', 'val'=>[80,75] }).run
+   r.table('hitPoints').insert({ :name=>'R', 'val'=>[75,70] }).run
+   r.table('hitPoints').insert({ :name=>'S', 'val'=>[100,95] }).run
+   r.table('hitPoints').insert({ :name=>'T', 'val'=>[95,90] }).run
+   r.table('hitPoints').insert({ :name=>'U', 'val'=>[90,85] }).run
+   r.table('hitPoints').insert({ :name=>'V', 'val'=>[85,80] }).run
+   r.table('hitPoints').insert({ :name=>'W', 'val'=>[80,75] }).run
+   r.table('hitPoints').insert({ :name=>'X', 'val'=>[75,70] }).run
+   r.table('hitPoints').insert({ :name=>'Y', 'val'=>[100,95] }).run
+   r.table('hitPoints').insert({ :name=>'Z', 'val'=>[95,90] }).run
+     
    bob = r.table('hitPoints').get_all(:name).run
    say = say + bob.inspect;
    event.respond say;      
