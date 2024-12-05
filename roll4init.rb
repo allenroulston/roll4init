1505c8d8392711b5f95e65c697e88cf2052d1d82
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-05 02:21:48 -0500

    getting closer to solved
       conn = PG.connect(ENV['DATABASE_URL'])
       conn.exec("DROP TABLE hitPoints");
       result = conn.exec("CREATE TABLE hitPoints (
                           name varchar(20),
                           fullHp integer,
                           nowHp integer)");

--- roll4init.rb
+++ roll4init.rb
@@ -44,43 +44,13 @@ bot.message(start_with: "SHIT") do |event|;
    say = "Here Come da Judge: \n";
 
    conn = PG.connect(ENV['DATABASE_URL'])
-#  PG.table_drop('hitPoints').run
-   conn.exec("CREATE TABLE hitPoints (
-        name varchar(20),
-        fullHp integer,
-        nowHp integer)");
-        
-=begin
-   r.table('hitPoints').insert({ :name=>'A', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'B', 'val'=>[95,90] }).run
-   r.table('hitPoints').insert({ :name=>'C', 'val'=>[90,85] }).run
-   r.table('hitPoints').insert({ :name=>'D', 'val'=>[85,80] }).run
-   r.table('hitPoints').insert({ :name=>'E', 'val'=>[80,75] }).run
-   r.table('hitPoints').insert({ :name=>'F', 'val'=>[75,70] }).run
-   r.table('hitPoints').insert({ :name=>'G', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'H', 'val'=>[95,90] }).run
-   r.table('hitPoints').insert({ :name=>'I', 'val'=>[90,85] }).run
-   r.table('hitPoints').insert({ :name=>'J', 'val'=>[85,80] }).run
-   r.table('hitPoints').insert({ :name=>'K', 'val'=>[80,75] }).run
-   r.table('hitPoints').insert({ :name=>'L', 'val'=>[75,70] }).run
-   r.table('hitPoints').insert({ :name=>'M', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'N', 'val'=>[95,90] }).run
-   r.table('hitPoints').insert({ :name=>'O', 'val'=>[90,85] }).run
-   r.table('hitPoints').insert({ :name=>'P', 'val'=>[85,80] }).run
-   r.table('hitPoints').insert({ :name=>'Q', 'val'=>[80,75] }).run
-   r.table('hitPoints').insert({ :name=>'R', 'val'=>[75,70] }).run
-   r.table('hitPoints').insert({ :name=>'S', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'T', 'val'=>[95,90] }).run
-   r.table('hitPoints').insert({ :name=>'U', 'val'=>[90,85] }).run
-   r.table('hitPoints').insert({ :name=>'V', 'val'=>[85,80] }).run
-   r.table('hitPoints').insert({ :name=>'W', 'val'=>[80,75] }).run
-   r.table('hitPoints').insert({ :name=>'X', 'val'=>[75,70] }).run
-   r.table('hitPoints').insert({ :name=>'Y', 'val'=>[100,95] }).run
-   r.table('hitPoints').insert({ :name=>'Z', 'val'=>[95,90] }).run
-=end
-   wut = conn.inspect;
+   conn.exec("DROP TABLE hitPoints");
+   result = conn.exec("CREATE TABLE hitPoints (
+                       name varchar(20),
+                       fullHp integer,
+                       nowHp integer)");
+  
    conn.close
-
    event.respond result.inspect;      
 end;
 ##################################################################################################################
