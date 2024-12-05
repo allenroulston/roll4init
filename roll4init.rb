6b79d809976977b51ff36efe7ed435493df7fa41
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-05 18:18:36 -0500

    method readData revised

--- roll4init.rb
+++ roll4init.rb
@@ -73,7 +73,7 @@ bot.message(start_with: "readData") do |event|;
    result = conn.exec("SELECT * FROM hitPoints");
    conn.close;
    say = " data Result \n";
-   say = say + result.inspect;   
+   say = say + result[PG][Result].to_s;   
    event.respond say;      
 end;
 #####################################################################
