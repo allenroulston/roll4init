87ad898e6308755520d93d11443101019de35903
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-06 10:49:29 -0500

    UBER delta method reVise
    was: sqlCode = "UPDATE hitPoints SET nowHp = 'rhp' WHERE name = 'Alpha';"
    now is: sqlCode = "UPDATE hitPoints SET nowHp = '55' WHERE name = 'Alpha';"

--- roll4init.rb
+++ roll4init.rb
@@ -80,7 +80,7 @@ bot.message(start_with: "reVise") do |event|;
      who = guy.fetch("name").to_s;
      rHp = guy.fetch("revHp").to_i;
      say = say + "Data: " + who.to_s + "   " + rHp.to_s + "  \n";
-     sqlCode = "UPDATE hitPoints SET nowHp = 'rHp' WHERE name = 'Alpha';"
+     sqlCode = "UPDATE hitPoints SET nowHp = '55' WHERE name = 'Alpha';"
   # Execute SQL update 
      conn.exec(sqlCode);
    end
