d02766887c2980332fa54a58435d7769639291ba
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-05 20:46:43 -0500

    update of method readData

--- roll4init.rb
+++ roll4init.rb
@@ -44,12 +44,12 @@ bot.message(start_with: "readData") do |event|;
    say = "The Data Result \n";
    conn = PG.connect(ENV['DATABASE_URL'])
    result = conn.exec("SELECT * FROM hitPoints");
-   theData = result.values;
-   (0..2).each do |x|;
-     say = say + theData[x]["name"].to_s + " \n";
-     say = say + theData[x]["fullHp"].to_s + " \n";
-     say = say + theData[x]["nowHp"].to_s  + " \n"; 
-   end;
+   say = result.values;
+   #(0..2).each do |x|;
+    # say = say + theData[x]["name"].to_s + " \n";
+    # say = say + theData[x]["fullHp"].to_s + " \n";
+    # say = say + theData[x]["nowHp"].to_s  + " \n"; 
+   #end;
    conn.close;   
    event.respond say;      
 end;
