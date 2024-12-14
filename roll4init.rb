7369e976b76652f06b4a87df1a3107c7a953696e
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-13 23:29:08 -0500

    method %r re-coded

--- roll4init.rb
+++ roll4init.rb
@@ -400,14 +400,14 @@ bot.message(start_with: "%r") do |event|;
     conn = PG.connect(ENV['DATABASE_URL'])
     idVal = 0;
     (0..36).each do |zed|
-        
-#.          say = say + "\n> " + idVal.to_s + " " + data[zed][6].to_s + " " + data[zed][1].to_s + " " + data[zed][2].to_s + " " + data[zed][4].to_s;
+            say = say + "\n" + idVal.to_s + "  " + data[zed][1].to_s + "  " + data[zed][2].to_s;
+#           say = say + "\n> " + idVal.to_s + " " + data[zed][6].to_s + " " + data[zed][1].to_s + " " + data[zed][2].to_s + " " + data[zed][4].to_s;
 #           r.table('activeInit').insert({ :id => idVal, :final => data[zed][6], :name => data[zed][1], :dex => data[zed][2], :status => data[zed][4] }).run;
-           sqlCode = "INSERT INTO activeInit (id, name, dexmod, adv, status, mixtape, final) VALUES (" +
-           idVal.to_s + ", '" + data[zed][1].to_s + "', " + data[zed][2].to_s + ", " + data[zed][3].to_s +
-           ", '" + data[zed][4] + "', '" + data[zed][5] + "', " + data[zed][6].to_s + ")";
-           say = say + "\n" + sqlCode;
-           result = conn.exec(sqlCode)
+##           sqlCode = "INSERT INTO activeInit (id, name, dexmod, adv, status, mixtape, final) VALUES (" +
+##           idVal.to_s + ", '" + data[zed][1].to_s + "', " + data[zed][2].to_s + ", " + data[zed][3].to_s +
+##           ", '" + data[zed][4] + "', '" + data[zed][5] + "', " + data[zed][6].to_s + ")";
+##           say = say + "\n" + sqlCode;
+##           result = conn.exec(sqlCode)
            idVal = idVal + 1;
     end;
     
