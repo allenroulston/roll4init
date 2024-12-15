2bed6132358f1a468ea25c94fcea0e53e8473ed0
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-14 19:06:33 -0500

    update to method %n

--- roll4init.rb
+++ roll4init.rb
@@ -451,7 +451,7 @@ bot.message(start_with: "%n") do |event|; #next initiative revealed
                  theID = item.fetch("id").to_s;
                  theName = item.fetch("name").to_s;
                  theFinal = item.fetch("final").to_s;
-                 say = say + "\n>[" + theID +"][" + theFinal +"] **" + theNAme + "**";
+                 say = say + "\n>[" + theID +"][" + theFinal +"] **" + theName + "**";
                  sqlCode = "DELETE FROM activeInit WHERE id = " + theID + ";";
                  conn.exec(sqlCode);
            end;
