884a0707e58f40e2114457f67c998e752e3434e7
Allen Roulston <allenroulston@users.noreply.github.com>
2024-12-16 13:33:58 -0500

    additional delta for method rolltheHP

--- roll4init.rb
+++ roll4init.rb
@@ -313,13 +313,12 @@ bot.message(start_with: "rolltheHP") do |event|;
                       theStatus = data[1].to_s; #acquire the status from position [1]
                       theDex = data[3].to_s; #acquire the Dexterity Modifier from position [3]
                   end;
-                  theName = manyNames[theID][rand(0..19)];
-                  say = say + "\n *-*-* " + theName.inspect + "\n";                  
-#           sqlCode = "UPDATE hitPoints SET name = " + theName + " lowhp = " + totalHP.to_s + ", maxhp = " + totalHP.to_s + ", status = '" + 
-#                     theStatus + "' , dexmod = " + theDex.to_s + " WHERE id = " + theID.to_s + ";";
-#           say = say + "\n" + sqlCode;
+                  theName = manyNames[theID][rand(0..19)]; # pull a name from the large selection of names                
+           sqlCode = "UPDATE hitPoints SET name = " + theName + " lowhp = " + totalHP.to_s + ", maxhp = " + totalHP.to_s + ", status = '" + 
+                     theStatus + "' , dexmod = " + theDex.to_s + " WHERE id = " + theID.to_s + ";";
+           say = say + "\n" + sqlCode;
          # Execute SQL update 
-         #  conn.exec(sqlCode);           
+           conn.exec(sqlCode);           
        end;
     else;
        say = "Input Error: " + event.content;
